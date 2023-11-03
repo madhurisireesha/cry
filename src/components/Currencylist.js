3e45eacee5b92136a99d073bebbf3b5b89fc0f6f
siri <madhurisireesha6@gmail.com>
2023-11-03 19:24:15 +0530

    modify

---
+++ src/components/Currencylist.js
@@ -0,0 +1,7 @@
+import '../styles/clist.css'
+const Currencylist=()=>{
+    return(
+        <img src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png" alt="bgimage" className='bg'/>
+    )
+}
+export default Currencylist
